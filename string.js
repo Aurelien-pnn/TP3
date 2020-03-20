5b09c612262af7b437a07cc4734f2b5df28a60dd
Aurelien-pnn <aurelien.pinon@isen-ouest.yncrea.fr>
2020-03-20 09:04:07 +0100

    exercice1

---
+++ string.js
@@ -0,0 +1,14 @@
+//Exercice 1
+
+function repeat(s, n){
+    let string = "";
+    let concat = s;
+    for(let i = 0; i < n; i++){
+        string = string + concat;
+    }
+
+    return string;
+}
+
+//Exercice 2
+
